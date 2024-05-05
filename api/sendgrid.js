7b96fe6d0db4514acdb8e0b420309d3b5052c8cf
Andreas Skovsted <113526100+Sk0vsted@users.noreply.github.com>
2024-05-05 18:36:59 +0200

    changed setApiKey in sendgrid.js

--- api/sendgrid.js
+++ api/sendgrid.js
@@ -1,31 +1,28 @@
 const sendgridMail = require("@sendgrid/mail");
-
-sendgridMail.setApiKey(process.env.SENDGRID_API_KEY); // Ensure this is set in Vercel's environment variables
+sendgridMail.setApiKey(process.env.REACT_APP_SENDGRID_API_KEY);
 
 module.exports = async (req, res) => {
-  // Ensure we're dealing with a POST request
-  if (req.method === "POST") {
-    // Parse the incoming JSON payload
-    const { fornavn, efternavn, email, telefon, besked } = JSON.parse(req.body);
+  if (req.method !== "POST") {
+    res.status(405).json({ error: "Method Not Allowed" });
+    return;
+  }
 
-    const content = {
-      to: "pau@pksmedeservice.dk",
-      from: "pksmedeservice.kontakt@gmail.com",
-      subject: `Ny Besked fra ${fornavn} ${efternavn}`,
-      text: `Fornavn: ${fornavn}\nEfternavn: ${efternavn}\nEmail: ${email}\nTelefon: ${telefon}\nBesked: ${besked}`,
-      html: `<p>Email: ${email}</p><p>Telefon: ${telefon}</p><p>Besked: ${besked}</p>`,
-    };
+  const { fornavn, efternavn, email, telefon, besked } = req.body;
 
-    try {
-      await sendgridMail.send(content);
-      return res.status(200).json({ message: "Email sent successfully" });
-    } catch (error) {
-      console.error("Error sending email", error);
-      return res
-        .status(500)
-        .json({ error: "Error sending email", details: error.toString() });
-    }
-  } else {
-    return res.status(405).json({ error: "Method Not Allowed" });
+  const content = {
+    to: "pau@pksmedeservice.dk",
+    from: "pksmedeservice.kontakt@gmail.com",
+    subject: `Ny Besked fra ${fornavn} ${efternavn}`,
+    text: `Fornavn: ${fornavn}\nEfternavn: ${efternavn}\nEmail: ${email}\nTelefon: ${telefon}\nBesked: ${besked}`,
+  };
+
+  try {
+    await sendgridMail.send(content);
+    res.status(200).json({ message: "Email sent successfully" });
+  } catch (error) {
+    console.error("Error sending email", error);
+    res
+      .status(500)
+      .json({ error: "Error sending email", details: error.toString() });
   }
 };
