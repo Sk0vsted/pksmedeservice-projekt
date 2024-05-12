3ed92ebfecc3c586b944f9961c1465d0dd1b4a8b
Sk0vsted <as.kristiansen10@hotmail.com>
2024-05-12 22:32:44 +0200

    updated sendgrid

--- api/sendgrid.js
+++ api/sendgrid.js
@@ -2,6 +2,10 @@ const sendgridMail = require('@sendgrid/mail');
 sendgridMail.setApiKey(process.env.REACT_APP_SENDGRID_API_KEY);
 
 module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   if (req.method !== 'POST') {
     res.status(405).json({ error: 'Method Not Allowed' });
     return;
