ce14a47cb1b3eb0108de3cea0a5e23e20c283f01
Sk0vsted <as.kristiansen10@hotmail.com>
2024-05-12 21:55:28 +0200

    updated sendgrid.js

--- api/sendgrid.js
+++ api/sendgrid.js
@@ -1,28 +1,36 @@
-const sendgridMail = require("@sendgrid/mail");
+const sendgridMail = require('@sendgrid/mail');
 sendgridMail.setApiKey(process.env.REACT_APP_SENDGRID_API_KEY);
 
 module.exports = async (req, res) => {
-  if (req.method !== "POST") {
-    res.status(405).json({ error: "Method Not Allowed" });
-    return;
+  // Ensure that we're dealing with a POST request
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method Not Allowed' });
   }
 
-  const { fornavn, efternavn, email, telefon, besked } = req.body;
+  // Parse the JSON body manually
+  let data = '';
+  req.on('data', (chunk) => {
+    data += chunk;
+  });
 
-  const content = {
-    to: "pau@pksmedeservice.dk",
-    from: "pksmedeservice.kontakt@gmail.com",
-    subject: `Ny Besked fra ${fornavn} ${efternavn}`,
-    text: `Fornavn: ${fornavn}\nEfternavn: ${efternavn}\nEmail: ${email}\nTelefon: ${telefon}\nBesked: ${besked}`,
-  };
+  req.on('end', async () => {
+    try {
+      const { fornavn, efternavn, email, telefon, besked } = JSON.parse(data);
 
-  try {
-    await sendgridMail.send(content);
-    res.status(200).json({ message: "Email sent successfully" });
-  } catch (error) {
-    console.error("Error sending email", error);
-    res
-      .status(500)
-      .json({ error: "Error sending email", details: error.toString() });
-  }
+      const content = {
+        to: 'pau@pksmedeservice.dk',
+        from: 'pksmedeservice.kontakt@gmail.com',
+        subject: `Ny Besked fra ${fornavn} ${efternavn}`,
+        text: `Fornavn: ${fornavn}\nEfternavn: ${efternavn}\nEmail: ${email}\nTelefon: ${telefon}\nBesked: ${besked}`,
+      };
+
+      await sendgridMail.send(content);
+      res.status(200).json({ message: 'Email sent successfully' });
+    } catch (error) {
+      console.error('Error sending email', error);
+      res
+        .status(500)
+        .json({ error: 'Error sending email', details: error.toString() });
+    }
+  });
 };
