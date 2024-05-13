a10fee9e853a80cff04147a61cbb25256345261f
Andreas Skovsted <113526100+Sk0vsted@users.noreply.github.com>
2024-05-13 15:58:45 +0200

    Update sendgrid.js

--- api/sendgrid.js
+++ api/sendgrid.js
@@ -2,44 +2,31 @@ const sendgridMail = require("@sendgrid/mail");
 sendgridMail.setApiKey(process.env.REACT_APP_SENDGRID_API_KEY);
 
 module.exports = async (req, res) => {
-  // Sæt CORS headers
-  res.setHeader("Access-Control-Allow-Origin", "*"); // Overvej at begrænse dette til specifikke domæner i produktion
-  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-  // Håndter OPTIONS metode for CORS preflight anmodninger
-  if (req.method === "OPTIONS") {
-    return res.status(204).end();
-  }
-
-  // Tjek at anmodningen er en POST
   if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method Not Allowed" });
+    res.status(405).json({ error: "Method Not Allowed" });
+    return;
   }
 
-  // Parse body data som JSON
-  let data = "";
-  req.on("data", (chunk) => {
-    data += chunk.toString(); // konverter Buffer til string
-  });
+  const { fornavn, efternavn, email, telefon, besked } = req.body;
 
-  req.on("end", async () => {
-    try {
-      const { fornavn, efternavn, email, telefon, besked } = JSON.parse(data);
-      const content = {
-        to: "pau@pksmedeservice.dk",
-        from: "pksmedeservice.kontakt@gmail.com",
-        subject: `Ny Besked fra ${fornavn} ${efternavn}`,
-        text: `Fornavn: ${fornavn}\nEfternavn: ${efternavn}\nEmail: ${email}\nTelefon: ${telefon}\nBesked: ${besked}`,
-      };
+  const content = {
+    to: "pau@pksmedeservice.dk",
+    from: "pksmedeservice.kontakt@gmail.com",
+    subject: `Ny Besked fra ${fornavn} ${efternavn}`,
+    text: `Fornavn: ${fornavn}\nEfternavn: ${efternavn}\nEmail: ${email}\nTelefon: ${telefon}\nBesked: ${besked}`,
+  };
 
-      await sendgridMail.send(content);
-      res.status(200).json({ message: "Email sent successfully" });
-    } catch (error) {
-      console.error("Error sending email", error);
-      res
-        .status(500)
-        .json({ error: "Error sending email", details: error.toString() });
-    }
-  });
+  try {
+    await sendgridMail.send(content);
+    res.status(200).json({ message: "Email sent successfully" });
+  } catch (error) {
+    console.error("Error sending email", error);
+    res
+      .status(500)
+      .json({ error: "Error sending email", details: error.toString() });
+  }
 };
